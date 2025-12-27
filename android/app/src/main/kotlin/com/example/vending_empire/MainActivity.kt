9db87ecd0d8e21efb571931a946c3881935dd72a
Hyejun <leehj825@gmail.com>
2025-12-27 08:37:50 -0800

    Remove MainActivity.kt files from both app namespaces to streamline project structure and eliminate redundancy.

--- android/app/src/main/kotlin/com/example/vending_empire/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.vendingempire.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/vendingempire/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.vendingmachiinetycoon.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
